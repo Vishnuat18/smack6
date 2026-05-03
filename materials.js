4389bf7a3139f168d9f636ea1bd2ce20fa1025b5
Vishnuat18 <vishnurajan24766@gmail.com>
2026-05-03 15:43:10 +0530

    5

--- materials.js
+++ materials.js
@@ -245,7 +245,8 @@ export const subjectMaterials = {
         ],
         materials: [
             { name: 'ESIA Comprehensive', file: 'esia/notes/ESIA_merged.pdf' },
-            { name: 'Unit 1 & 2: Intro & Assessment', file: 'esia/notes/esia_notes.pdf' },
+            { name: 'Unit 1: Introduction', file: 'esia/notes/esia_u1.pdf' },
+            { name: 'Unit 2: Environmental Assessment', file: 'esia/notes/esia_u2.pdf' },
             { name: 'Unit 3: Environmental Management Plan', file: 'esia/notes/esia_u3.pdf' },
             { name: 'Unit 4: Socio Economic Assessment', file: 'esia/notes/esia_u4.pdf' },
             { name: 'Unit 5: Case Studies', file: 'esia/notes/esia_u5.pdf' },
