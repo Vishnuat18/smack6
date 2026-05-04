580d3ef9cd978818b1c6615caf82b36ffc279274
Vishnuat18 <vishnurajan24766@gmail.com>
2026-05-04 18:24:46 +0530

    7

--- materials.js
+++ materials.js
@@ -244,11 +244,11 @@ export const subjectMaterials = {
             }
         ],
         materials: [
-            { name: 'Unit 1: Introduction', file: 'esia/notes/esia_u1.pdf', isUpdated: true },
-            { name: 'Unit 2: Environmental Assessment', file: 'esia/notes/esia_u2.pdf', isUpdated: true },
-            { name: 'Unit 3: Environmental Management Plan', file: 'esia/notes/esia_u3.pdf', isUpdated: true },
-            { name: 'Unit 4: Socio Economic Assessment', file: 'esia/notes/esia_u4.pdf', isUpdated: true },
-            { name: 'Unit 5: Case Studies', file: 'esia/notes/esia_u5.pdf', isUpdated: true },
+            { name: 'Unit 1: Introduction', file: 'esia/notes/esia_u1.pdf' },
+            { name: 'Unit 2: Environmental Assessment', file: 'esia/notes/esia_u2.pdf' },
+            { name: 'Unit 3: Environmental Management Plan', file: 'esia/notes/esia_u3.pdf' },
+            { name: 'Unit 4: Socio Economic Assessment', file: 'esia/notes/esia_u4.pdf' },
+            { name: 'Unit 5: Case Studies', file: 'esia/notes/esia_u5.pdf' },
             { name: '2-Mark Question Bank', file: 'esia/2m/esia_2m.pdf' }
         ],
         previousYearQuestions: [
